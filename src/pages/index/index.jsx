71eb75a93f065c721bd931a3e2ad185ec933978b
facundo3336 <fcv3336@gmail.com>
2022-06-14 11:57:21 -0300

    add case section mobile version in index page

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -26,6 +26,34 @@ export const Index = () => {
           </Link>
         </div>
       </section>
+      <section className="caseSection">
+        <div className="sideSpacing">
+          <h2>Case Studies</h2>
+          <Link to="/">
+            <Button colorClass="violet">View Case Studies</Button>
+          </Link>
+          <div
+            className="caseImageBackground"
+            style={{
+              backgroundImage: `url("http://c.files.bbci.co.uk/12CB3/production/_119897967_gettyimages-946828612.jpg")`,
+            }}
+          >
+            <div className="caseBlackBackgroundContainer">
+              <h3>Gen M</h3>
+            </div>
+          </div>
+          <div
+            className="caseImageBackground"
+            style={{
+              backgroundImage: `url("https://media.istockphoto.com/photos/boiling-water-tap-in-kitchen-picture-id1207766632?k=20&m=1207766632&s=612x612&w=0&h=zlMjx4jp3zTIziBV2iBErwSbqGZcZwEIuNLDufj4ku4=")`,
+            }}
+          >
+            <div className="caseBlackBackgroundContainer">
+              <h3>Hot Watter Taps</h3>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
